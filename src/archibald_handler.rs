022e489181c48b08438f287a92bfa07100b8629c
Daniel Cuthbert <daniel@seventysix.io>
2022-08-05 12:32:38 +0100

    added a static_content dir to serve default files from there. It works, sorta
    Signed-off-by: Daniel Cuthbert <daniel@seventysix.io>

--- src/archibald_handler.rs
+++ src/archibald_handler.rs
@@ -18,9 +18,29 @@ use super::http::{methods, requests, response, statuscodes};
 // use super::http::StatusCode;
 use super::http::statuscodes::StatusCode::{JollyGood, NotFound};
 use super::server::archibaldserver::ServerHandler;
+use std::fs;
 
 // this is the main handler module
-pub struct ArchibaldHandler;
+// This is for the static_files directory where we serve content from the filesystem.
+pub struct ArchibaldHandler {
+    static_path: String,
+}
+
+// In order to serve the basic index.html page, we need a new handler
+
+impl ArchibaldHandler {
+    pub fn new<T: Into<String>>(static_path: T) -> Self {
+        Self {
+            // a new handler with the static_path
+            static_path: static_path.into(),
+        }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.static_path, file_path); // reads the public path
+        fs::read_to_string(path).ok() //this will read the file and if there is an error tell us about it. ok() looks at the result and if good converts the value into an option
+    }
+}
 
 impl ServerHandler for ArchibaldHandler {
     // this handles the request
@@ -30,7 +50,7 @@ impl ServerHandler for ArchibaldHandler {
             // We need to handle the requests depending on what they are. This is where we do that.
             Allowedmethods::GET => match request.path() {
                 // If the path is /, we want to return a simple string
-                "/" => Response::new(JollyGood, Some("I say old boy!!".to_string())),
+                "/" => Response::new(JollyGood, self.read_file("index.html")),
                 _ => Response::new(NotFound, Some("Not Found".to_string())),
             },
             // If the query is None, we return a 404 error
--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,15 @@ use server::archibaldserver::Server;
 mod archibald_handler;
 mod http;
 mod server;
+use std::env;
 
 // Start of the main function
 fn main() {
+    // before we start, we need to read environment variables
+    // for now, we need to pass this like so: ARCHIBALD_STATIC_PATH=~/Code/Archibald/static_content cargo run
+    let default_path = format!("{}/static_content", env!("CARGO_MANIFEST_DIR")); // this is the path to the cargo manifest directory
+    let static_path = env::var("ARCHIBALD_STATIC_PATH").unwrap_or(default_path); // this is the path to the static content
+    println!("STATIC PATH: {}", static_path);
     // We need to tell it what to listen on. We'll use the default port, but we can change it
     // We should support IPv6 and IPv4 at the same time. We'll use the default IPv4 for now
     // we need to convert the ip/port to a string
@@ -25,7 +31,8 @@ fn main() {
     let _post = Allowedmethods::POST;
 
     // Start the server, this will always run
-    archibald.run(ArchibaldHandler);
+    // we need to read the path from an environment variable
+    archibald.run(ArchibaldHandler::new(static_path));
 }
 
 // In order to accept and process incoming requests, we need to store them somewhere.
